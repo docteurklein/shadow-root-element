cb07da76dd3a2cc7554f6d256e723ba50b3f1cc8
Klein Florian <florian.klein@free.fr>
2019-11-13 13:19:28 +0100

    div only
    Signed-off-by: Klein Florian <florian.klein@free.fr>

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 export default class HTMLShadowRootElement extends HTMLElement {
-  constructor() {
-    super();
-    this.shadow = this.attachShadow({mode: 'open', delegatesFocus: true});
-  }
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open', delegatesFocus: true});
+    }
 
-  connectedCallback() {
-    Array.from(this.childNodes).forEach(child => this.shadow.appendChild(this.removeChild(child)));
-  }
+    connectedCallback() {
+        Array.from(this.childNodes).forEach(child => this.shadowRoot.appendChild(this.removeChild(child)));
+    }
 }
 customElements.define('shadow-root', HTMLShadowRootElement);
